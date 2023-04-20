58ea5ef687a632781d97353e4e11b33e7311c7e3
DESKTOP-KOGKS86\84273 <842731@naver.com>
2023-04-20 16:13:40 +0900

    feat.CodeUp 1014

---
+++ src/main/java/CodeUp/code1014.java
@@ -0,0 +1,15 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class code1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("");
+        char first = sc.next().charAt(0);
+        System.out.print("");
+        char second = sc.next().charAt(0);
+
+        System.out.println("" + second + " " + first);
+    }
+}
