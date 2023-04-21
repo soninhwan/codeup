00d17d4c4150df224689d377cbdd2c7c163e6b6e
In Hwan Son <842731@naver.com>
2023-04-21 10:53:47 +0900

    feat.CodeUp 1017

--- src/main/java/CodeUp/Code1017.java
+++ src/main/java/CodeUp/Code1017.java
@@ -3,8 +3,8 @@ package CodeUp;
 import java.util.Scanner;
 public class Code1017 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        System.out.printf("%d %d %d", num, num, num);
+        Scanner sc = new Scanner(System.in);
+        int val = sc.nextInt();
+        System.out.printf("%d %d %d", val, val, val);
     }
 }
